1eadd065e00cd79bc7c1dea0dbe4a2b7738e82b7
Ranjan8294 <ranjankumar29122001@gmail.com>
2025-11-28 17:11:54 +0530

    Frontend Deployment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://apnavideocallbackend-kdx0.onrender.com" :
 
     "http://localhost:8000"
 
